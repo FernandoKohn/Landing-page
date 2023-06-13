124ebe2bfd22957fdf021a2f6108b39e7ffabe19
FernandoKohn <contatofernandokohn@gmail.com>
2023-06-13 17:57:44 -0300

    Dia 10 13/06/2023
    - Aba de skills
    - Mudança do script para um arquivo externo

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+import Typed from 'typed.js';
 
-var cursor = document.getElementById("cursor")
+var cursor = document.getElementById("cursor");
 
 document.onmousemove = function (e) {
-    cursor.style.left = (e.pageX + 1.5) + "px";
-    cursor.style.top = (e.pageY + 1.5) + "px";
+    cursor.style.left = (e.pageX - 1.8) + "px";
+    cursor.style.top = (e.pageY - 2) + "px";
     cursor.style.display = "block";
 }
 
@@ -13,4 +14,17 @@ var typed = new Typed(".Efeito-digitado", {
     backSpeed: 100,
     backDelay: 1000,
     loop: true
-})
+});
+
+function abrirjanela() {
+    if (document.querySelector(".habilidades-janela").style.display == "block") {
+        document.querySelector(".habilidades-janela").style.display = "none"
+    } else {
+        document.querySelector(".habilidades-janela").style.display = "block"
+    }
+
+};
+
+function fecharjanela() {
+    document.querySelector(".habilidades-janela").style.display = "none"
+};
