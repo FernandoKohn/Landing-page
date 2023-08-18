09a88da23e559eddd2c3e3227b892ca94b073ffc
Fernando Kohn <contatofernandokohn@gmail.com>
2023-08-17 23:01:02 -0300

    🥳🥳 SITE PUBLICADO COM SUCESSO 🥳🥳
    PRIMEIRO PROJETO FINALIZADO
    DEPOIS DE MUITO TRABALHO ARDUO
    OLHA ONDE CHEGAMOS COM MUITO ESTUDO E DEDICAÇÃO!
    
    DATA DA PUBLICAÇÃO: 16/08/2023
    
    AGRADECIMENTOS ESPECIAIS: ZIG
    
    ---------
    
    As alterações seguem:
    
    Botões das redes sociais ajeitados para resolução baixa ( a posição estava muito colada no botão sobre )
    
    Caixa de email agora movel igual a janela de habilidades
    
    Codigo do dark mode reduzido consideravelmente utilizando QuerySelectorAll
    
    Inicializador iniciado

---
+++ scripts/inicializador.js
@@ -0,0 +1,11 @@
+var inicializador = document.querySelector(".inicializador") 
+const corpo = document.querySelector(main)
+
+
+function removerInicializador() {
+    return inicializador.style.display = "none"
+}
+
+function Inicializador() {
+    
+}
