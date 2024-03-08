d183e479295fdb58c5db7c31e39c2a460403ed23
Fernando Kohn <contatofernandokohn@gmail.com>
2024-03-08 16:30:15 -0300

    mudanças iniciadas

--- js/script.js
+++ js/script.js
@@ -615,4 +615,31 @@ document.querySelector("body").addEventListener("animationend", () => {
     document.querySelector("#cursor").style.animation = ""
 })
 
+const slider = document.querySelector('.projetos-container');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+});
+
 //skywars.net
